43a17fa0bcad01ffb5043b3606f95064d5c81953
Sailor Winkelman <sailorwinkelman@gmail.com>
2017-12-26 19:52:19 -0800

    Refactoring: change type of input to number, remove related script warning user to enter a number, add comments, change index to i and use i++

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,31 @@ var countNumber = function(number) {
 	for (var i = 1; i <= number; i++){
 		if (i % 15 === 0){
 			countedNumbers.push("ping-pong");
+		} else if (i % 5 === 0){
+			countedNumbers.push("pong");
+		} else if (i % 3 === 0){
+			countedNumbers.push("ping");
 		} else {
 			countedNumbers.push(i);
 		}
-	}
+	} //end for loop started on 5
 
 };
 //front end logic
 $(document).ready(function() {
   $("form#blank").submit(function() {
     event.preventDefault();
-    var number = parseInt($("input#number").val());
+    var number = $("input#number").val();
 		console.log(number);
     if (isNaN(number) === true) {
-      $("#not").text("please enter a number");
+      $("#not").text("please enter a number u fool");
     };
 
     countNumber(number);
 		console.log(countedNumbers);
 
     countedNumbers.forEach(function(number) {
-      $("#output").append("<li>" + number + "kittens" + "</li>");
+      $("#output").append("<li>" + number + "</li>");
     });
   });
 });
