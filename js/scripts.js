e61ba25bddb9219900778f0b09eb21d83056d9d1
Sailor Winkelman <sailorwinkelman@gmail.com>
2017-12-26 16:41:01 -0800

    Refactor countNumber function. Add console log testers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 //business logic
 var countedNumbers = [];
 
-var countNumber = function(Number) {
+var countNumber = function(number) {
+	for (var i = 1; i <= number; i++){
+		if (i % 15 === 0){
+			countedNumbers.push("ping-pong");
+		} else {
+			countedNumbers.push(i);
+		}
+	}
 
 };
 //front end logic
@@ -9,15 +16,16 @@ $(document).ready(function() {
   $("form#blank").submit(function() {
     event.preventDefault();
     var number = parseInt($("input#number").val());
-
+		console.log(number);
     if (isNaN(number) === true) {
       $("#not").text("please enter a number");
     };
 
     countNumber(number);
+		console.log(countedNumbers);
 
     countedNumbers.forEach(function(number) {
-      $("#output").append("<li>" + number + "</li>");
+      $("#output").append("<li>" + number + "kittens" + "</li>");
     });
   });
 });
